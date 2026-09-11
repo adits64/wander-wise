09dc2759f3a2689910dd8e4dda97ca154db07e70
Aditya Singh <adits.rok.64@gmail.com>
2026-09-11 12:55:39 +0545

    installed cors

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,16 @@ import connectDB from './config/database.js';
 import HANDLERS from './handlers/index.js';
 import errorMiddleware from './middlewares/error.js';
 import { authMiddleware } from './middlewares/auth.js';
-
+import cors from cors;
 const app = express();
 
 const PORT = process.env.PORT;
 
 connectDB();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(authMiddleware);
